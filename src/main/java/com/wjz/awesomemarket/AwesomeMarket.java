14490a6daa2172673f92bb3c1ebf680c2e260bc8
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-01-21 09:00:03 +0800

    1

---
+++ src/main/java/com/wjz/awesomemarket/AwesomeMarket.java
@@ -0,0 +1,17 @@
+package com.wjz.awesomemarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AwesomeMarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
